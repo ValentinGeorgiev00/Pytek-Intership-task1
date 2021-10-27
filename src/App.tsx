719597a963178fe0f61a24d7339899cdff2f9518
ValentinGeorgiev00 <val.georgiev.00@gmail.com>
2021-10-27 12:49:42 +0200

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import "./App.css";
 
 import { FC, useState } from "react";
 
-type question = { key: string; value: string; isOpen: boolean };
+type question = { id: number; key: string; value: string; isOpen: boolean };
 const App: FC = () => {
   const [questions, setQuestions] = useState<question[]>([
-    { key: "Question number 0", value: "answer0", isOpen: false },
-    { key: "Question number 1", value: "answer1", isOpen: false },
-    { key: "Question number 2", value: "answer2", isOpen: false },
-    { key: "Question number 3", value: "answer3", isOpen: false },
-    { key: "Question number 4", value: "answer4", isOpen: false },
-    { key: "Question number 5", value: "answer5", isOpen: false },
-    { key: "Question number 6", value: "answer6", isOpen: false },
-    { key: "Question number 7", value: "answer7", isOpen: false },
-    { key: "Question number 8", value: "answer8", isOpen: false },
-    { key: "Question number 9", value: "answer9", isOpen: false },
+    { id: 0, key: "Question number 0", value: "answer0", isOpen: false },
+    { id: 1, key: "Question number 1", value: "answer1", isOpen: false },
+    { id: 2, key: "Question number 2", value: "answer2", isOpen: false },
+    { id: 3, key: "Question number 3", value: "answer3", isOpen: false },
+    { id: 4, key: "Question number 4", value: "answer4", isOpen: false },
+    { id: 5, key: "Question number 5", value: "answer5", isOpen: false },
+    { id: 6, key: "Question number 6", value: "answer6", isOpen: false },
+    { id: 7, key: "Question number 7", value: "answer7", isOpen: false },
+    { id: 8, key: "Question number 8", value: "answer8", isOpen: false },
+    { id: 9, key: "Question number 9", value: "answer9", isOpen: false },
   ]);
 
   return (
@@ -23,8 +23,8 @@ const App: FC = () => {
         <h1>Frequently asked questions</h1>
         <h3>Hello! Didn't find what you are looking for? Please contact us.</h3>
       </div>
-      {questions.map((element, pos) => (
-        <section key={pos}>
+      {questions.map((element) => (
+        <section key={element.id}>
           <div className="container">
             <div className="box">
               <div className="box-item" id="question1">
