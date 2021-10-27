e2b61de7be8966daebf9969a9be26f1e37596ed3
ValentinGeorgiev00 <val.georgiev.00@gmail.com>
2021-10-27 10:26:38 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,21 @@
 import "./App.css";
 
-import { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import { FC, useState } from "react";
 
-interface CreateQuestionBoxProps {
-  element: { key: string; value: string; index: number };
-  idAnother: string;
-  setIdAnother: Dispatch<SetStateAction<string>>;
-}
-
-const CreateQuestionBox: FC<CreateQuestionBoxProps> = ({
-  element,
-  idAnother,
-  setIdAnother,
-}) => {
-  const [currentExtended, setCurrentExtended] = useState<string>("");
-  const [isExtended, setIsExtended] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (isExtended) {
-      setIdAnother(`${element.index}`);
-    }
-  }, [isExtended, element.index, element.key, setIdAnother]);
-
-  return (
-    <section>
-      <div className="container">
-        <div className="box">
-          <div className="box-item" id="question1">
-            <div
-              className="box-link"
-              onClick={() => {
-                setCurrentExtended(`${element.index}`);
-                setIsExtended(!isExtended);
-              }}
-            >
-              <div> {element.key}</div>
-              <div className="box-icon-wrapper">
-                <div className="icon">
-                  {currentExtended === idAnother
-                    ? isExtended
-                      ? "▲"
-                      : "▼"
-                    : "▼"}
-                </div>
-              </div>
-            </div>
-            <div
-              className={
-                currentExtended === idAnother
-                  ? isExtended
-                    ? "answer-expanded"
-                    : "answer"
-                  : "answer"
-              }
-            >
-              <p>{element.value}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
+type question = { key: string; value: string; isOpen: boolean };
 const App: FC = () => {
-  const [idAnother, setIdAnother] = useState<string>("");
-
-  const quest = [
-    { key: "question1", value: "answer1" },
-    { key: "question2", value: "answer2" },
-    { key: "question3", value: "answer3" },
-  ];
+  const [questions, setQuestions] = useState<question[]>([
+    { key: "Question number 0", value: "answer0", isOpen: false },
+    { key: "Question number 1", value: "answer1", isOpen: false },
+    { key: "Question number 2", value: "answer2", isOpen: false },
+    { key: "Question number 3", value: "answer3", isOpen: false },
+    { key: "Question number 4", value: "answer4", isOpen: false },
+    { key: "Question number 5", value: "answer5", isOpen: false },
+    { key: "Question number 6", value: "answer6", isOpen: false },
+    { key: "Question number 7", value: "answer7", isOpen: false },
+    { key: "Question number 8", value: "answer8", isOpen: false },
+    { key: "Question number 9", value: "answer9", isOpen: false },
+  ]);
 
   return (
     <div>
@@ -78,12 +23,36 @@ const App: FC = () => {
         <h1>Frequently asked questions</h1>
         <h3>Hello! Didn't find what you are looking for? Please contact us.</h3>
       </div>
-      {quest.map((element, index) => (
-        <CreateQuestionBox
-          element={{ index, ...element }}
-          idAnother={idAnother}
-          setIdAnother={setIdAnother}
-        />
+      {questions.map((element) => (
+        <section>
+          <div className="container">
+            <div className="box">
+              <div className="box-item" id="question1">
+                <div
+                  className="box-link"
+                  onClick={() => {
+                    setQuestions((prevState) =>
+                      prevState.map((item) => {
+                        if (item.key === element.key) {
+                          return { ...element, isOpen: !item.isOpen };
+                        }
+                        return { ...item, isOpen: false };
+                      })
+                    );
+                  }}
+                >
+                  <div> {element.key}</div>
+                  <div className="box-icon-wrapper">
+                    <div className="icon">{element.isOpen ? "▲" : "▼"}</div>
+                  </div>
+                </div>
+                <div className={element.isOpen ? "answer-expanded" : "answer"}>
+                  <p>{element.value}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       ))}
     </div>
   );
